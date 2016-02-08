aa7c1632eca33f7afa7c1e30dd0289038a6bbd57
SeraphicScribe <channaka.developer@gmail.com>
2016-02-08 17:01:31 +0530

    Base App

---
+++ EmpRegApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EmpRegApp.Startup))]
+namespace EmpRegApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
