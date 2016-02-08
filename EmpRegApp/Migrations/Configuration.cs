9cdba35262def2c03f62d4f1ebce34706e60c9a1
SeraphicScribe <channaka.developer@gmail.com>
2016-02-09 04:21:39 +0530

    Added canEdit Role Logic and added code to seed method

--- EmpRegApp/Migrations/Configuration.cs
+++ EmpRegApp/Migrations/Configuration.cs
@@ -5,6 +5,8 @@ namespace EmpRegApp.Migrations
     using System.Data.Entity.Migrations;
     using System.Linq;
     using EmpRegApp.Models;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
 
     internal sealed class Configuration : DbMigrationsConfiguration<EmpRegApp.Models.ApplicationDbContext>
     {
@@ -14,20 +16,28 @@ namespace EmpRegApp.Migrations
             AutomaticMigrationDataLossAllowed = true;
         }
 
+        bool AddUserAndRole(EmpRegApp.Models.ApplicationDbContext context)
+        {
+            IdentityResult ir;
+            var rm = new RoleManager<IdentityRole>
+                (new RoleStore<IdentityRole>(context));
+            ir = rm.Create(new IdentityRole("canEdit"));
+            var um = new UserManager<ApplicationUser>(
+                new UserStore<ApplicationUser>(context));
+            var user = new ApplicationUser()
+            {
+                UserName = "user1@live.com",
+            };
+            ir = um.Create(user, "passworD321");
+            if (ir.Succeeded == false)
+                return ir.Succeeded;
+            ir = um.AddToRole(user.Id, "canEdit");
+            return ir.Succeeded;
+        }
+
         protected override void Seed(EmpRegApp.Models.ApplicationDbContext context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            AddUserAndRole(context);
 
             context.Employees.AddOrUpdate(p => p.nicNumber,
                new Employee
